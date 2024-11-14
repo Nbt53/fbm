7ffc79a2b09d6af7aca012a3b7dd92b1bf8907d3
nbt53 <nbt853.nh@gmail.com>
2024-11-14 07:55:48 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fbm',
+  webDir: 'dist'
+};
+
+export default config;
